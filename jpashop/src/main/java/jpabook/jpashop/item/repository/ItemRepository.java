aff65cea9f0e3693784c4e9de574d378cd424299
greenn-lab <greenn.lab@gmail.com>
2019-06-12 22:14:07 +0900

    completed ch13

--- jpashop/src/main/java/jpabook/jpashop/item/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package jpabook.jpashop.item.repository;
-
-import jpabook.jpashop.item.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
